125f22b479d5abe894adfe90c96fc73bd2d28d79
rlevilla42 <rlevilla42@gmail.com>
2024-11-04 02:12:37 +0100

    bon ca a l'air de marcher si je fais docker exec -it mariadb mysql -u root -p

--- srcs/requirements/wordpress/wp-config.sh
+++ srcs/requirements/wordpress/wp-config.sh
@@ -5,17 +5,17 @@ mkdir -p /var/www/html
 cd /var/www/html
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-if ! ./wp-cli.phar core is-installed --allow-root  ; then
-    ./wp-cli.phar core download --allow-root
-    ./wp-cli.phar config create --dbname=db_wordpress --dbuser=buakaw --dbpass=buakaw --dbhost=mariadb --allow-root --force
-    ./wp-cli.phar core install --url=rlevilla.42.fr --title=wptitle --admin_user=buakaw --admin_password=buakaw --admin_email=caca@gmail.com --allow-root
-fi
-
 #if ! ./wp-cli.phar core is-installed --allow-root  ; then
 #    ./wp-cli.phar core download --allow-root
-#    ./wp-cli.phar config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASS --dbhost=$MYSQL_HOST --allow-root --force
-#    ./wp-cli.phar core install --url=$WP_URL --title=$WP_BLOG_TITLE --admin_user=$WP_ADMIN_USER --admin_password=buakaw --admin_email=caca@gmail.com --allow-root
+#    ./wp-cli.phar config create --dbname=db_wordpress --dbuser=buakaw --dbpass=buakaw --dbhost=mariadb --allow-root --force
+#    ./wp-cli.phar core install --url=rlevilla.42.fr --title=wptitle --admin_user=buakaw --admin_password=buakaw --admin_email=caca@gmail.com --allow-root
 #fi
+
+if ! ./wp-cli.phar core is-installed --allow-root  ; then
+    ./wp-cli.phar core download --allow-root
+    ./wp-cli.phar config create --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASS --dbhost=$MYSQL_HOST --allow-root --force
+    ./wp-cli.phar core install --url=$WP_URL --title=$WP_BLOG_TITLE --admin_user=$WP_ADMIN_USER --admin_password=${WP_ADMIN_PASS} --admin_email=caca@gmail.com --allow-root
+fi
 
 if ! ./wp-cli.phar core is-installed --allow-root  ; then
     echo "Wordpress is installed and running"
